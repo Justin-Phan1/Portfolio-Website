73c80d94bfc4dac1b333827a6bc475ad0203f423
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-08-03 23:26:27 -0400

    Add icons and links to LinkedIn and GitHub accounts

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,8 @@
 import './index.scss'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className="nav-bar">
@@ -16,6 +17,18 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul> 
+            <li>
+                <a target="_blank" rel="noreferrer" href="https:www.linkedin.com/in/justin-phan-/"> 
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a> 
+            </li>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https:www.github.com/Justin-Phan1"> 
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a> 
+            </li>
+        </ul>
     </div>
 )
 
