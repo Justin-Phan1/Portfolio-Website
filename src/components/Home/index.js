b488204fca816a05d8d99d85e525fd2cc8ce5b5a
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-08-04 01:14:46 -0400

    Implement AnimateLetters to text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,29 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
+import { useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['J', 'u', 's', 't', 'i', 'n']
+    const jobArray = ['s', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'e', 'n', 'g', 'i', 'n', 'e', 'e', 'r', '.']
+
     return (
         <div className="container home-page"> 
             <div className="text-zone"> 
-                <h1>Hi, 
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _ 12`}>i,</span>
                     <br /> 
-                    I'm Justin,
+                    <span className={`${letterClass} _ 13`}>I',</span>
+                    <span className={`${letterClass} _ 14`}>m</span>
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={nameArray}
+                    index={15} />,
                     <br /> 
-                    software engineer.
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={jobArray}
+                    index={22} />.
                 </h1>
                 <h2>Software Engineering Student at Stevens Institute of Technology</h2>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
