6c1f620e0436cc64b1b52d1dfea1ea53ee132dd7
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-08-04 00:09:04 -0400

    Add text for Home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,17 @@
+import { Link } from 'react-router-dom';
+import './index.scss';
+
 const Home = () => {
     return (
         <div className="container home-page"> 
-            <div className="text-zone"> </div>
+            <div className="text-zone"> 
+                <h1>Hi, <br /> I'm Justin,</h1>
+                <br /> 
+                software engineer
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Home
