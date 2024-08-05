86d16afe2b76792f0eed1ed42bb3203282672903
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-08-05 18:20:02 -0400

    Implement page Loader

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import Loader from 'react-loaders';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -20,6 +21,7 @@ const Home = () => {
     }, []);
 
     return (
+        <>
         <div className="container home-page"> 
             <div className="text-zone"> 
                 <h1>
@@ -40,6 +42,8 @@ const Home = () => {
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
             </div>
         </div>
+        <Loader type="ball-pulse-sync" />
+        </>
     );
 }
 
