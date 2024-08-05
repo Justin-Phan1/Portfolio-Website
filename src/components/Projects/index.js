b14fe70bd5e9b615bf720caebfcb386639403405
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-08-05 18:43:04 -0400

    Add Loader for Projects page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,10 @@
 import './index.scss'
+import Loader from 'react-loaders'
 
 const Projects = () => {
-    
+    return (
+        <Loader type="ball-pulse-sync" />
+    )
 }
 
 export default Projects
