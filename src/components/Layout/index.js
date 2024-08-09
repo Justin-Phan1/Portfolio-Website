769ee5749199b9723a049a334bc158ba47b7bcb0
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-08-09 00:15:48 -0400

    Update Layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,16 @@
 import './index.scss';
+import Sidebar from '../Sidebar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
-    return <>Hello</>
+    return (
+    <div className="App">
+        <Sidebar /> 
+        <div className="page">
+            <Outlet />      
+        </div>
+    </div>
+    )
 }
 
 export default Layout
